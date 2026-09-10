1059738890ab3238fa24e800e2a6efb75cb4ccad
Marionna Saunders <131487069+Msaunders2@users.noreply.github.com>
2026-09-09 19:06:13 -0500

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        let queue = [root];
+        let levelSize = queue.length;
+        let res = [];
+
+        if(!root){
+            return res;
+        }
+
+        while(queue.length > 0){
+            levelSize = queue.length;
+
+            for(let i = 0; i < levelSize; i++){
+                let node = queue.shift()
+
+                if(node.left){
+                    queue.push(node.left);
+                }
+                if(node.right){
+                    queue.push(node.right);
+                }
+
+                if(i === (levelSize - 1)){
+                    res.push(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
