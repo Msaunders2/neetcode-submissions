5cf2bdbfcfd2e3981690af3da730d7b9fc2cca8f
Marionna Saunders <131487069+Msaunders2@users.noreply.github.com>
2026-09-21 18:22:20 -0500

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.js
@@ -0,0 +1,43 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head) {
+        if (!head) return null;
+        let map = new Map();
+        let curr = head;
+
+        while(curr){
+            map.set(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+        while(curr){
+            if(curr.next !== null){
+                map.get(curr).next = map.get(curr.next);
+            }else{
+                map.get(curr).next = null;
+            }
+
+            if(curr.random !== null){
+                map.get(curr).random = map.get(curr.random);
+            }else{
+                map.get(curr).random = null;
+            }
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+
+}
