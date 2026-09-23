d099839df20f566358f8aad966530c836809ddf3
Marionna Saunders <131487069+Msaunders2@users.noreply.github.com>
2026-09-23 18:38:33 -0500

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let res = [];
+        let curr = [];
+
+        if(nums.length === 0 || target === 0){
+            return res;
+        }
+        let currSum = 0;
+        backtrack(0);
+
+        //think this is mostly right but the currSum is not the right parameter 
+        
+        function backtrack(start){
+            if(currSum === target){
+                res.push([...curr].sort((a,b) => a-b));
+                return;
+            }
+
+            for(let i = start; i < nums.length; i++){
+                if(currSum > target){
+                    continue;
+                }
+
+                curr.push(nums[i])
+                currSum += nums[i]
+                backtrack(i);
+                curr.pop();
+                currSum -= nums[i];
+
+            }
+
+        }
+
+        return res;
+    }
+}
