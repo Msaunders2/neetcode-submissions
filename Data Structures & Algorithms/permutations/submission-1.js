f328a752b5fc7b537ee7c4ca81984097220bbc1e
Marionna Saunders <131487069+Msaunders2@users.noreply.github.com>
2026-09-23 19:44:31 -0500

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        let res = [];
+        let curr = [];
+
+        if(nums.length === 0){
+            return res;
+        }
+
+        backtrack(0);
+
+        function backtrack(start){
+            if(curr.length === nums.length){
+                res.push([...curr]);
+            }
+
+            for(let i = 0; i < nums.length; i++){
+                //violation
+                if(curr.includes(nums[i])){
+                    continue;
+                }
+
+                //make choice
+                curr.push(nums[i]);
+
+                //backtrack
+                backtrack(i);
+
+                //undo choice
+                curr.pop();
+            }
+
+        }
+
+        return res;
+    }
+}
